d8ce4c58d74899723a89cbf51ba9d1ad8762c0b0
Caleb Klinger <Klingercaleb11@gmail.com>
2023-07-28 12:41:40 -0700

    Added project 5

--- Project 5/MIPScode.asm
+++ Project 5/MIPScode.asm
@@ -288,9 +288,91 @@ syscall      # system call (terminate)
 .text
 main:
 # -----------------------
-li $t0,33
+li $t0,56
 
-li $t1,133.0
+li $t1,149.0
+
+move $a0,$t0
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t0
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t1
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t1
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+# -----------------
+#  Done, terminate program.
+
+li $v0,10   # call code for terminate
+syscall      # system call (terminate)
+.end main
+.text
+main:
+# -----------------------
+li $t0,56
+
+li $t1,149.0
+
+move $a0,$t0
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t0
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t1
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+move $a0,$t1
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $a0, 10
+li $v0, 11
+syscall
+
+# -----------------
+#  Done, terminate program.
+
+li $v0,10   # call code for terminate
+syscall      # system call (terminate)
+.end main
+.text
+main:
+# -----------------------
+li $t0,56
+
+li $t1,149.0
 
 move $a0,$t0
 li $v0,1   # call code for terminate
